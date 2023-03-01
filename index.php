08baf676c5686ee3c2323218da6d309fdb269851
jguerreiro <joel.borghi@gmail.com>
2023-03-01 18:50:03 -0300

    feature/refactor-01 - Adding fun things

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +9,10 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Shantell+Sans:ital@1&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/_global.css">
+  <script src="js/video.js"></script>
   <title>Calculator</title>
 </head>
+
 <body>
   <div class="container-calculator">
     <div class="container-display">
@@ -41,7 +44,12 @@
       <div class="btn">%</div>
       <div class="btn">+</div>
     </form>
-    </div>
   </div>
+  </div>
+  <video autoplay muted loop id="myVideo">
+    <source src="assets/background-vid.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+  </video>
 </body>
+
 </html>
